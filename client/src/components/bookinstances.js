3b86562cf8245ef9be109f32842e140e725ba0fe
alizoubair <ali.zoubair@outlook.com>
2022-06-15 17:01:08 +0100

    Add bookinstance pages

--- client/src/components/bookinstances.js
+++ client/src/components/bookinstances.js
@@ -1,39 +1,42 @@
 import React ,{ Component } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 export default class BookInstances extends Component {
 	constructor(props) {
 		super(props)
+		this.BookInstance = this.BookInstance.bind(this);
+		this.BookInstances = this.BookInstances.bind(this);
 		this.state = {
 			bookInstances: []
 		}
 	}
 
-	componentDidMount() {
+	componentDidUpdate() {
 		axios.get('http://localhost:3001/catalog/bookinstances')
 			.then(res => {
 				console.log(res.data);
-				this.setState({ bookInstances: res.data });
+				this.setState({ bookInstances: res.data })
 			})
-			.catch(function (error) {
-				console.log(error);
+	}
+	
+	BookInstance(props) {
+		return <li>{props.bookInstance}</li>
+	}
+
+	BookInstances() {
+		const bookInstances = this.state.bookInstances;
+		if (bookInstances) {
+			return bookInstances.map(bookInstance => {
+				return <this.BookInstance key={bookInstance.id} bookInstance={bookInstance} />
 			})
-	};
+		}
+	}
 
 	render() {
-		const { bookInstances } = this.state.bookInstances;
-
 		return (
 			<div>
 				<h1>Book Instance List</h1>
- 				<ul> 
- 					{ 
- 						bookInstances.map(bookinstance => {
- 							<li><Link>{bookinstance.id}</Link></li>
- 						})
- 					} 
- 				 </ul>
+				<ul><this.BookInstances /></ul>
 			</div>
 		)
 	}
