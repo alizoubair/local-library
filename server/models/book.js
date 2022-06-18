5d335aca71ee80717bda2fc1f98f1fdd1d7a40f6
alizoubair <ali.zoubair@outlook.com>
2022-06-18 18:18:42 +0100

    Add book schema

--- server/models/book.js
+++ server/models/book.js
@@ -11,7 +11,7 @@ const bookSchema = new mongoose.Schema(
 )
 
 bookSchema.set('toJSON', {
-    transform: (document, requiredObject) => {
+    transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
